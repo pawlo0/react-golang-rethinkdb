8f09fff1cc06469f7c0fd07eacd2686abd10ab04
Paulo Santos <Paulo_sss@hotmail.com>
2016-11-12 18:52:14 +0000

    React Introduction - Creating your First React Component

---
+++ app.js
@@ -0,0 +1,9 @@
+class Channel extends React.Component{
+    render(){
+        return(
+            <li>Channel Name</li>
+        );
+    }
+}
+
+ReactDOM.render(<Channel />, document.getElementById('app'));
