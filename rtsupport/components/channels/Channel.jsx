95b396577935171a2b84ebec5a9505d7ac08cdb8
Paulo Santos <Paulo_sss@hotmail.com>
2016-11-18 07:40:04 +0000

    Create the Channel List Component

---
+++ rtsupport/components/channels/Channel.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class Channel extends Component{
+    onClick(e){
+        e.prevetnDefault();
+        const {setChannel, channel} = this.props;
+        setChannel(channel);    
+    }
+    render(){
+        const {channel} = this.props;
+        return(
+            <li>
+                <a onClick={this.onClick.bind(this)}>
+                    {channel.name}
+                </a>
+            </li>
+        );
+    }
+}
+
+Channel.propTypes = {
+    channel: React.propTypes.object.isRequired,
+    setChannel: React.propTypes.func.isRequires
+};
+
+export default Channel;
